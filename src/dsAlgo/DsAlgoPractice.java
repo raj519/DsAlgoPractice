07d31cbda0017e6706c701595518cbbd772eab6a
Rajnish <rajnishchandansdet@gmail.com>
2024-10-11 16:15:20 +0530

    second commit

---
+++ src/dsAlgo/DsAlgoPractice.java
@@ -0,0 +1,11 @@
+package dsAlgo;
+
+public class DsAlgoPractice {
+
+	public static void main(String[] args) {
+		String s= "Practice datastructure and algorithm";
+		System.out.println(s);
+
+	}
+
+}
